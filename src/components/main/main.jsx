1ae704ce5400080490e9789812a350458e536acc
YuraBarykin <yBarykin@gmail.com>
2023-10-14 12:03:34 +0400

    Дабавлен поиск без учета регистра

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -7,58 +7,63 @@ import Data from '../list.json'
 import './main.css'
 
 const Main = () => {
-    const [inputValue, setInputValue] = React.useState('')
-    const [select, setSelect] = React.useState([])
-    const [list, setList] = React.useState(Data.List)
-    const [filterList, setFilterList] = React.useState(Data.List)
+  const [inputValue, setInputValue] = React.useState('')
+  const [select, setSelect] = React.useState([])
+  const [list, setList] = React.useState(Data.City)
+  const [filterList, setFilterList] = React.useState(Data.City)
 
-    const clearSelectList = () => {
-        setSelect([])
-        setList(Data.List)
-        setFilterList(Data.List)
-    }
+  const clearSelectList = () => {
+    setSelect([])
+    setList(Data.City)
+    setFilterList(Data.City)
+  }
 
-    const change = (value) => {
-        setInputValue(value)
-        const itemInclude = list.filter(item => {
-            return item.text.toLowerCase().includes(value.toLowerCase())
-        })
+  const change = (value) => {
+    setInputValue(value)
+    const itemInclude = list.filter(item => {
+      return item.text.toLowerCase().includes(value.toLowerCase())
+    })
 
-        setFilterList(itemInclude.map(item => {
-                    const newText = item.text.replace(value, '<b>' + value + '</b>')
-                    return {...item, text: newText}
-                } 
-            )
-        )
-    }
-
-    const selectItem = (item) => {
-        setSelect([...select, ...Data.List.filter(a => a.id === item.id)])
-        setList([...list.filter(a => a.id !== item.id)])
-        setFilterList(filterList.filter(a => a.id !== item.id))
-        setInputValue('')
-    }
-
-    return (
-        <div className="main">
-            <SelectList 
-                list={select}
-                clearList={clearSelectList}
-            />
-            <div className="main__search-block">
-                <input 
-                    className="main__input" 
-                    type="text" 
-                    onChange={(e) => change(e.target.value)}
-                    value={inputValue}
-                />
-                <SearchList 
-                    list={filterList}
-                    onClick={selectItem}
-                />
-            </div>
-        </div>
+    setFilterList(itemInclude.map(item => {
+          return {...item, text: selectionText(item.text, value)}
+        } 
+      )
     )
+  }
+
+  const selectItem = (item) => {
+    setSelect([...select, ...Data.City.filter(a => a.id === item.id)])
+    setList([...list.filter(a => a.id !== item.id)])
+    setFilterList(filterList.filter(a => a.id !== item.id))
+    setInputValue('')
+  }
+
+  return (
+    <div className="main">
+      <SelectList 
+        list={select}
+        clearList={clearSelectList}
+      />
+      <div className="main__search-block">
+        <input 
+          className="main__input" 
+          type="text" 
+          onChange={(e) => change(e.target.value)}
+          value={inputValue}
+        />
+        <SearchList 
+          list={filterList}
+          onClick={selectItem}
+        />
+      </div>
+    </div>
+  )
+}
+
+const selectionText = (str, text) => {
+  const regexp = new RegExp(text, 'ig')
+
+  return str.replace(regexp, '<b>' + text + '</b>')
 }
 
 export default Main
